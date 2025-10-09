24329892d2a133c92d8bfa71dd451dd02edabed8
jobprepacademyuk-design <jobprepacademyuk@gmail.com>
2025-10-09 17:50:46 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,10 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+import AuthPage from '@/pages/Auth';
+import AccountPage from '@/pages/Account';
+
+// inside <Routes>
+<Route path="/auth" element={<AuthPage />} />
+<Route path="/account" element={<AccountPage />} />
