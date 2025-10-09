0d64b825a812d3734c53a02548c30e9b93f097cf
jobprepacademyuk-design <jobprepacademyuk@gmail.com>
2025-10-09 18:07:03 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
+// src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-import AuthPage from '@/pages/Auth';
-import AccountPage from '@/pages/Account';
-
-// inside <Routes>
-<Route path="/auth" element={<AuthPage />} />
-<Route path="/account" element={<AccountPage />} />
